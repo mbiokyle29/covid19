7fc92f462fbcb3f6f0fafba3109a4e89d538651c
Kyle McChesney <mbio.kyle@gmail.com>
2020-03-16 14:25:16 -0600

    gene + feature tracks

--- vis/pages/index.js
+++ vis/pages/index.js
@@ -7,11 +7,33 @@ export default function index() {
   useEffect(() => {
     var options = {
       reference: {
-          id: "NC_045512.2",
-          name: "SARS-CoV-2",
-          fastaURL: "./igv/ref/NC_045512.2.fa",
-          indexURL: "./igv/ref/NC_045512.2.fa.fai",
-      }
+        id: "NC_045512.2",
+        name: "SARS-CoV-2",
+        fastaURL: "./igv/ref/NC_045512.2.fa",
+        indexURL: "./igv/ref/NC_045512.2.fa.fai",
+      },
+      tracks: [
+        {
+          name: "Genes",
+          type: "annotation",
+          format: "gff3",
+          url: "./igv/ref/NC_045512.2.genes.gff",
+          nameField: "gene",
+          displayMode: "EXPANDED",
+          color: "#32a8a0",
+          autoHeight: true
+        },
+        {
+          name: "Features",
+          type: "annotation",
+          format: "gff3",
+          url: "./igv/ref/NC_045512.2.features.gff",
+          nameField: "gene",
+          displayMode: "EXPANDED",
+          color: "#d67282",
+          autoHeight: true
+        }
+      ]
     };
     import('igv').then((igv) => igv.createBrowser(ref.current, options).then(function (browser) {
       console.log("Created IGV browser");
